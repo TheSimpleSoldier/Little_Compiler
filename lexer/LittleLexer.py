88f02b030ca572369dda861b1bed9816b1f50ae6
Joshua Leger <legermjoshua@gmail.com>
2016-02-05 15:28:02 -0700

    added comments

--- lexer/LittleLexer.py
+++ lexer/LittleLexer.py
@@ -1,6 +1,7 @@
 import lex
 import sys
 
+#All the reserved words
 reserved = [
     'PROGRAM',
     'BEGIN',
@@ -22,6 +23,7 @@ reserved = [
     'FLOAT'
 ]
 
+#All the possible tokens
 tokens = [
     'STRINGLITERAL',
     'FLOATLITERAL',
@@ -31,32 +33,45 @@ tokens = [
     'KEYWORD'
 ]
 
+#Strings are a " followed by some number of non ", followed by a "
 t_STRINGLITERAL = r'"[^"]*"'
+#Possible operators are <=, >=, :=, +, -, *, /, =, !=, <, >, (, ), ;, ','
 t_OPERATOR = r'<=|>=|:=|\+|-|\*|/|=|!=|<|>|\(|\)|;|,'
+#Floats are 0 or more numbers, a ., and 1 or more numbers
 t_FLOATLITERAL = r'[0-9]*\.[0-9]+'
+#Ints are 1 or more numbers
 t_INTLITERAL = r'[0-9]+'
 
+#Identifiers are variables and keywords.
+#Variables are a letter followed by 0 or more letters and numbers
+#Keywords are listed at the top
 def t_IDENTIFIER(t):
     r'[a-zA-Z][a-zA-Z0-9]*'
     if t.value in reserved:
         t.type = 'KEYWORD'
     return t
 
+#Comments are a -- followed by the rest of the line
 def t_COMMENT(t):
     r'--.*'
     pass
 
+#This will pass over any character that doesn't fit the other rules
+#It is mainly used to skip over whitespace characters
 def t_error(t):
     t.lexer.skip(1)
     pass
 
+#Create lexer
 lexer = lex.lex()
 
 
+#Reads in the file and processes it
 with open(sys.argv[1], "r") as myFile:
     data = myFile.read()
     lexer.input(data)
 
+#For each token found, prints it out
 for tok in lexer:
     print("Token Type: " + tok.type)
     print("Value: " + str(tok.value))
