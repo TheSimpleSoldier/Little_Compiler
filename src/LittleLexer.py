865503861d88f9e18bbd81ac07fa387284d57a24
Joshua Leger <legermjoshua@gmail.com>
2016-04-25 19:47:01 -0600

    moved symbol table to other file

--- src/LittleLexer.py
+++ src/LittleLexer.py
@@ -5,6 +5,7 @@ import sys
 import lex
 import yacc
 from IRRep import *
+from SymbolTable import *
 import itertools
 
 error = False
@@ -110,130 +111,6 @@ def t_error(t):
     pass
 
 littleLexer = lex.lex()
-
-class SymbolTable:
-    def __init__(self, name, parent):
-        self.name = name
-        self.variables = []
-        self.children = []
-        self.parent = parent
-
-    def getValues(self):
-        return self.variables
-
-    def clear(self):
-        self.variables = []
-
-    def addChild(self, child):
-        self.children.append(child)
-
-    def insertChild(self, child, after):
-        if after == "":
-            self.children.insert(0, child)
-            return
-
-        count = 0
-        for c in self.children:
-            count += 1
-            if c.name == after:
-                self.children.insert(count, child)
-                return
-        self.children.append(child)
-
-    def addVariable(self, variable):
-        global error, duplicate
-        if not self.contains(variable):
-            self.variables.append(variable)
-        elif not error:
-            error = True
-            duplicate = variable.name
-
-    def contains(self, variable):
-        for var in self.variables:
-            if var.name == variable.name:
-                return True
-
-        return False
-
-    def containsName(self, name):
-        for var in self.variables:
-            if var.name == name:
-                return True
-        return False
-
-
-    def insertVar(self, variable, varIndex):
-        global error, duplicate
-        if not self.contains(variable):
-            self.variables.insert(varIndex, variable)
-        elif not error:
-            error = True
-            duplicate = variable.name
-
-    def getLength(self):
-        return len(self.variables)
-
-    def printSymbolTable(self):
-        print "Symbol table " + self.name
-        for var in self.variables:
-            if (var.hasValue()):
-                print "name " + var.name + " type " + var.v_type+ " value " + var.value
-            else:
-                print "name " + var.name + " type " + var.v_type
-
-        for child in self.children:
-            print ""
-            child.printSymbolTable()
-
-    def printTableTiny(self):
-        for var in self.variables:
-            if(var.v_type == "STRING"):
-                print "str " + var.name + " " + var.value
-            else:
-                print "var " + var.name
-
-    def getType(self, name):
-        for var in self.variables:
-            if(var.name == name):
-                return var.v_type
-        return None
-
-    def getParent(self):
-       return self.parent
-
-
-    def getValueForVar(self, name):
-        for v in self.varaibles:
-            if v.name == name:
-                return v
-        return self.parent.getValueForVar(name)
-
-class Variable:
-    def __init__(self, name, v_type):
-        self.name = name
-        self.v_type = v_type
-        self.has_value = False
-
-    def hasValue(self):
-        return self.has_value
-
-    def setValue(self, value):
-        self.value = value
-        self.has_value = True
-
-
-globalSymbolTable = SymbolTable("GLOBAL", None)
-globalTempSymbolTable = SymbolTable("GLOBAL", None)
-currentScope = globalSymbolTable
-currentScope.parent = currentScope
-lastType = None
-index = -1
-currentBlock = 1
-parameterList = SymbolTable("Params", None)
-funcList = SymbolTable("Funcs", None)
-blockList = SymbolTable("Block", None)
-lastFunc = ""
-lastBlock = ""
 
 # Parsing rules
 
@@ -821,6 +698,19 @@ def p_error(p):
     global error
     error = True
 
+globalSymbolTable = SymbolTable("GLOBAL", None)
+globalTempSymbolTable = SymbolTable("GLOBAL", None)
+currentScope = globalSymbolTable
+currentScope.parent = currentScope
+lastType = None
+index = -1
+currentBlock = 1
+parameterList = SymbolTable("Params", None)
+funcList = SymbolTable("Funcs", None)
+blockList = SymbolTable("Block", None)
+lastFunc = ""
+lastBlock = ""
+
 yacc.yacc()
 
 irlist = IRRep()
@@ -837,8 +727,3 @@ with open(sys.argv[1], "r") as myFile:
 nodeLast = IRNode("RET", "", "", "", None, None)
 irlist.addToEnd(nodeLast)
 irlist.printTiny(globalSymbolTable)
-
-#if error:
-#    print("DECLARATION ERROR " + duplicate)
-#else:
-#    globalSymbolTable.printSymbolTable()
